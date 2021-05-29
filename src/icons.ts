4abb65ccba14ad0249a1e9a42e69666e68ae1505
Leonard Grosoli <hileo77@gmail.com>
2021-05-29 19:21:10 +0200

    add icons

--- src/icons.ts
+++ src/icons.ts
@@ -1,30 +1,33 @@
-import { MapCellName } from './map';
-import placeholder from './assets/cellPlaceholderIcon.png'
-import water from './assets/cellWaterIcon.png'
+import { MapCellName, MapCellTexturePos } from './map';
+import resourceIcons from './assets/resourceIcons.png'
+import cellIcons from './assets/cellIcons.png'
+import buildingIcons from './assets/buildingIcons.png'
+import { ResourceName, ResourcesEnum } from './resources';
+import { toPx } from './utils';
+import { BUILDING_ICONS_PER_ROW, BUILDING_ICON_SIZE, MAP_CELL_ICONS_PER_ROW, MAP_CELL_ICON_SIZE, RESOURCE_ICONS_PER_ROW, RESOURCE_ICON_SIZE } from './globalConstants';
+import { BuildingEnum, BuildingNames } from './building';
 
-export const cellIcons: { [key in MapCellName]: string } = {
-  'gras': placeholder,
-  'forest': placeholder,
-  'stone': placeholder,
-  'clay': placeholder,
-  'bush': placeholder,
-  'berries': placeholder,
-  'tin': placeholder,
-  'copper': placeholder,
-  'gold': placeholder,
-  'iron': placeholder,
-  'water': water,
-  'water coast': placeholder,
-  'water coast 2': placeholder,
-  'water coast 3': placeholder,
-  'water coast 4': placeholder,
-  'water pond': placeholder,
-  'placeholder_0': placeholder,
-  'placeholder_1': placeholder,
-  'placeholder_2': placeholder,
-  'placeholder_3': placeholder,
-  'placeholder_4': placeholder,
-  'placeholder_5': placeholder,
-  'placeholder_6': placeholder,
-  'placeholder_7': placeholder,
+export function getResourceIcon(type: ResourceName) {
+  return getGenericIcon(ResourcesEnum[type], RESOURCE_ICONS_PER_ROW, RESOURCE_ICON_SIZE, resourceIcons);
+}
+
+export function getCellIcon(type: MapCellName) {
+  return getGenericIcon(MapCellTexturePos[type], MAP_CELL_ICONS_PER_ROW, MAP_CELL_ICON_SIZE, cellIcons);
+}
+
+export function getBuildingIcon(type: BuildingNames) {
+  return getGenericIcon(BuildingEnum[type], BUILDING_ICONS_PER_ROW, BUILDING_ICON_SIZE, buildingIcons);
+}
+
+function getGenericIcon(i: number, perRow: number, size: number, src: string) {
+  const el = document.createElement('span')
+  const x = i % perRow;
+  const y = Math.floor(i / perRow);
+  el.style.backgroundImage = `url(${src})`;
+  el.style.backgroundPositionX = toPx(x * -size);
+  el.style.backgroundPositionY = toPx(y * -size);
+  el.style.height = toPx(size);
+  el.style.width = toPx(size);
+  el.style.display = 'inline-block';
+  return el
 }
